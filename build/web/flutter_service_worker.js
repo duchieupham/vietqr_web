49597586e88cba8dbf07e1c793a19211ca8ac4b1
admin <admin@admins-MacBook-Pro-2.local>
2023-04-13 23:16:34 +0700

    build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e5e961cc88c45665b8b6be7bd0ea317b",
-"index.html": "dbaf0ca7a93452cb37d1fc2ad35718b0",
-"/": "dbaf0ca7a93452cb37d1fc2ad35718b0",
+"index.html": "8f3817f8d98f61ada3f41b88b9e9dcc9",
+"/": "8f3817f8d98f61ada3f41b88b9e9dcc9",
 "firebase-messaging-sw.js": "ac580569feefa4089ae00ab086b89ada",
 "main.dart.js": "fc83b989094744a3c717f0f015a3e4eb",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
