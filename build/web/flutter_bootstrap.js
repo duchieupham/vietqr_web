51b5dc33f6f9096361e3eab415ab8e4f188abbe0
hieuvh.vietqr <hieu.vh@vietqr.vn>
2024-10-24 11:07:20 +0700

    hot fix

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1320606314"
+    serviceWorkerVersion: "2237385394"
   }
 });
